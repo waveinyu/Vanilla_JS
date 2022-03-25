4e85a9d192e2803aae0641e606d64198af6471b6
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-03-25 19:22:38 +0900

    Updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ console.log("Your new name is " + myName);
 
 //boolean
 const amICute = true;
-let something;
+let something; //undefined
 console.log(amICute);
 
 //Arrays
@@ -43,3 +43,58 @@ console.log(player.cute);
 console.log(player.points);
 player.points = player.points + 15;
 console.log(player.points);
+
+function sayHello(nameOfPerson, age) {
+  console.log(
+    "Hello, my name is " + nameOfPerson + " and I'm " + age + " olds."
+  );
+}
+
+const nct = {
+  name: "Doyoung",
+  age: 27,
+  unit: "nct 127",
+  sayHello: function (otherPersonName) {
+    console.log("Hello! " + otherPersonName + ", Nice to meet you.");
+  },
+};
+
+console.log(nct.name);
+nct.sayHello("jaehyun");
+
+console.log(nct.name);
+nct.name = "Mark";
+console.log(nct.name);
+
+console.log(nct.unit);
+nct.unit = "nct dream";
+console.log(nct.unit);
+
+console.log(nct);
+
+const nct2020 = ["doyoung", "jaehyun", "haechan", "jungwoo"];
+console.log(nct2020);
+nct2020.push("johnny");
+console.log(nct2020);
+nct.age = nct.age - 3;
+console.log(nct.age);
+
+// function : 계속 반복해서 사용할 수 있는 코드 조각
+// NaN : Not a Number
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+plus(2, 5);
+
+function minus(a, b) {
+  console.log(a - b);
+}
+
+minus(5, 1);
+
+function divide(a, b) {
+  console.log(a / b);
+}
+divide(30, 15);
