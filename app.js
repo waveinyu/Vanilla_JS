fd221ae2e2eb0cbd511a615002fd03e15badc1ef
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-03-29 18:36:26 +0900

    Update app.js and index.html

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-getElementById() : id 하나의 값을 반환
+const title = document.querySelector(".hello:first-child h1");
+console.dir(title);
 
-getElementsByClassName() : 클래스 네임, array에 할당
+//on- : events, 모두 JavaScript object
 
-getElementsByTagName() : array에 할당
+function handleTitleClick() {
+  title.style.color = "whitesmoke";
+  title.innerText = "title was clicked!";
+}
 
-그리고 querySelector()
- : CSS 셀렉터처럼 쓸 수 있기 때문에 매우 간편하고 유용하다
+function handleMouseEnter() {
+  title.innerText = "mouse is here!";
+}
 
- class, id 모두 사용 가능. 해당 클래스/id 특정 태그들 검색 가능
-하지만, 동일한 class가 있다면 전부를 가져오는 것이 아니라 가장 첫번째의 element만 가져온다.
+function handleMouseLeave() {
+  title.innerText = "mouse is gone!";
+}
 
-같은 조건의 것을 모두 가져오고 싶다면, querySelectorAll() 를 사용
-단, array의 상태로 할당!
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+//handleTitleClick에 괄호(실행버튼)를 넣지 않는 건 매우 중요
+//왜냐하면, 우리가 직접 실행하는 게 아니라 자바스크립트가 해주기를 바라는 것이기 때문!
 
-
-const title = document.querySelector(".hello h1");
-title.innerText = "hello";
+//event를 찾고 싶을 때? APIs가 들어간 게시글을 찾기
+//API : JavaScript관점의 html elements
