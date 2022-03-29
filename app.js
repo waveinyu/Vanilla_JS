437ed4e2d1f00f931118dc42a26693911b7781f0
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-03-29 18:06:00 +0900

    Update app.js and index.html

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
-// function is run from internal to external
-const age = parseInt(prompt("How old are you?"));
+getElementById() : id 하나의 값을 반환
 
-//isNaN returns type of boolean
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number");
-  // condition === true
-} else if (age < 18) {
-  console.log("You are too young to drink alcohol!");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink!");
-} else if (age > 50 && age <= 80) {
-  console.log("Think you health . . . ");
-} else if (age === 100) {
-  console.log("WOW You are wise");
-} else if (age > 80) {
-  console.log("You can do whatever you want . . .");
-}
+getElementsByClassName() : 클래스 네임, array에 할당
 
-true || true === true;
-true || false === true;
-false || true === true;
-false || false === false;
+getElementsByTagName() : array에 할당
 
-true && true === true;
-true && false === true;
-false && true === true;
-false && false === false;
+그리고 querySelector()
+ : CSS 셀렉터처럼 쓸 수 있기 때문에 매우 간편하고 유용하다
 
-if ((a && b) || (c && d) || (x && y)) {
-}
+ class, id 모두 사용 가능. 해당 클래스/id 특정 태그들 검색 가능
+하지만, 동일한 class가 있다면 전부를 가져오는 것이 아니라 가장 첫번째의 element만 가져온다.
 
-// java script is interacting with user
-// 클릭을 감지하는 방법, events 등등..
+같은 조건의 것을 모두 가져오고 싶다면, querySelectorAll() 를 사용
+단, array의 상태로 할당!
+
+
+const title = document.querySelector(".hello h1");
+title.innerText = "hello";
