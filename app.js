22c638a70e16e511a68434e300f3d4947252d673
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-03-31 15:24:53 +0900

    Update app.js, index.html and style.css

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "whitesmoke") {
-    newColor = "tomato";
-  } else {
-    newColor = "whitesmoke";
-  }
-  h1.style.color = newColor;
-}
-
-h1.addEventListener("click", handleTitleClick);
-
 // step 1. Seek elements
 // step 2. Listen event
 // step 3. React that event
+const h1 = document.querySelector("div.hello:first-child h1");
+
+//string을 변수에 저장하는 게 훨씬 좋다
+function handleTitleClick() {
+  const clickedClass = "clicked";
+  if (h1.className === "clickedClass") {
+    h1.className = "";
+  } else {
+    h1.className = "clickedClass";
+  }
+}
+
+//h1.className : getter이자 setter
+//"active" : String .. 오타로 인한 에러의 가능성이 많음 -> raw String
+
+h1.addEventListener("click", handleTitleClick);
