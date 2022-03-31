85219b7976bd97cd8798e7b19619cf06220d5c83
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-03-31 16:19:23 +0900

    Updated app.js, index.html and style.css

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// step 1. Seek elements
-// step 2. Listen event
-// step 3. React that event
 const h1 = document.querySelector("div.hello:first-child h1");
 
-//string을 변수에 저장하는 게 훨씬 좋다
 function handleTitleClick() {
   const clickedClass = "clicked";
-  if (h1.className === "clickedClass") {
-    h1.className = "";
+  // const clickedClass = "clicked sexy-font";
+  // 이 방법은 그다지 좋지 않다.
+
+  //classList가 clicked를 포함하고 있는지 확인하는 것
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = "clickedClass";
+    h1.classList.add(clickedClass);
+    //raw string 대신 const를 썼기 때문에 ""는 필요하지 않다.
   }
 }
 
-//h1.className : getter이자 setter
-//"active" : String .. 오타로 인한 에러의 가능성이 많음 -> raw String
-
 h1.addEventListener("click", handleTitleClick);
+//최초의 className이 사라지고, 우리는 sexy-font란 className를 간직하고 싶다.
+//sexy-font를 삭제하지 않고 clicked class를 변경하고 싶을 땐 어떻게 해야할까?
