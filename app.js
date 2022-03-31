5a19b53f07c0b5fee5d514d3f594041b8a635a61
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-03-31 16:32:53 +0900

    Updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  // const clickedClass = "clicked sexy-font";
-  // 이 방법은 그다지 좋지 않다.
-
-  //classList가 clicked를 포함하고 있는지 확인하는 것
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-    //raw string 대신 const를 썼기 때문에 ""는 필요하지 않다.
-  }
+  h1.classList.toggle("clicked");
+  //toggle은 classList에 clicked class가 이미 있는지 확인해서 만약 있다면,
+  //toggle이 clicked를 제거해주고,
+  //만일 clicked가 없다면 toggle이 clicked를 추가해준다.
 }
 
 h1.addEventListener("click", handleTitleClick);
-//최초의 className이 사라지고, 우리는 sexy-font란 className를 간직하고 싶다.
-//sexy-font를 삭제하지 않고 clicked class를 변경하고 싶을 땐 어떻게 해야할까?
