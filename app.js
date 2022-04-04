a3b704a3b561ad688fa55f814883724d47dd2639
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-04-04 16:00:35 +0900

    Update app.js and index.html

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+/*
+const loginForm = document.getElementById("");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+와 아래는 같다.
+*/
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log("Hello", loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
 }
 //Log In 버튼을 누르면 clicked가 뜨게 만들었다.
 //click event를 loginButton에 연결
 loginButton.addEventListener("click", onLoginBtnClick);
+
+/*
+validity of value
+값의 유효성을 확인하는 건 좋은 연습이다
+*/
