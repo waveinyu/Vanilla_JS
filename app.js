9d8310c530063363de13210feb38846f154ecb33
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-04-07 14:34:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 function onLoginSubmit(event) {
-  event.preventDefault(); //어떤 이벤트든지 액션이 발생하지 않게 막는 것
+  event.preventDefault();
   console.log(loginInput.value);
 }
-//누군가 form을 submit하면 브라우저는 기본적으로 페이지를 새로고침하게 되어있다.
-//preventDefault는 그 기본 동작을 막는 것
+/*
+onLoginSubmit function이 하나의 argument을 받도록 하고 그 정보를 JavaScript에 넘겨주고 있다
+우리가 argument 공간만 제공하면 JavaScript가 알아서 방금 일어난 event에 대한 정보를 지닌 argument를 채워넣을 것
+
+결과) 방금 실행된 event에 대한 여러 정보들을 담고 있다
+SubmitEvent {isTrusted: true, submitter: input, type: 'submit', 
+target: form#login-form, currentTarget: form#login-form, …}
+*/
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+// 아무것도 하지 않으믕로써 JavaScript가 어떤 정보를 담은 채로 function을 호출한다는 걸 배웠다
+// preventDefault() : 브라우저의 기본 동작을 막아준다
