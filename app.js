e1cca25e7098b372c46e7d5606047f87ec174754
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-04-08 15:32:05 +0900

    Update app.js and index.html

--- app.js
+++ app.js
@@ -3,19 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-//string만 포함되고, 그다지 중요한 내용이 아닌 변수는 대문자로 표기(관슴)
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event) {
+function onLoginForm(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  // greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username} 안녕`;
+  localStorage.setItem("USERNAME_KEY", username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username} ^.^`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-/*
-form은 사라졌지만 입력값은 console에 기록됐다.
-*/
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
+console.log(savedUsername);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginForm);
+} else {
+  paintGreetings(savedUsername);
+}
