173288393b0a4aa1a1b4b5ed31286aa7cae7d951
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-06-05 15:36:48 +0900

    Updated js

--- js/quote.js
+++ js/quote.js
@@ -49,7 +49,7 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-const todayQuote = quotes[parseInt(Math.random() * quotes.length)];
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todayQuote.quote;
 author.innerText = todayQuote.author;
